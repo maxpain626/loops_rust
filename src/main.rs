3f61e35d62ad959824bd89aebe44934b62fb9371
maxpain <greyhack01@rambler.ru>
2025-05-01 08:12:38 +0300

    counter+=1;

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        println!("{}", counter);
+        if counter >=10{
+            break counter*2;
+        }
+    };
+    print!("Результат равен: {}", result);
 }
